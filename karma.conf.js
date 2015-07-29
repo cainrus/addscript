00876473d2506e37fa13c77d106d3bdcfd05e484
Sergei Tarassov <cainrus@gmail.com>
2015-07-29 23:37:13 +0300

    optimizing karma config for saucelabs

--- karma.conf.js
+++ karma.conf.js
@@ -24,71 +24,65 @@ var customLaunchers = [
     {
         browserName: 'internet explorer',
         version: '7',
-        platform: 'Windows 2003'
+        platform: 'Windows XP'
         , base: 'SauceLabs'
     },
     {
         browserName: 'internet explorer',
         version: '8',
-        platform: 'Windows 2003'
+        platform: 'Windows XP'
         , base: 'SauceLabs'
     },
     {
         browserName: 'internet explorer',
         version: '9',
-        platform: 'Windows 2008'
+        platform: 'Windows 7'
         , base: 'SauceLabs'
     },
     {
         browserName: 'internet explorer',
         version: '10',
-        platform: 'Windows 2012'
+        platform: 'Windows 7'
         , base: 'SauceLabs'
     },
     {
         browserName: 'internet explorer',
         version: '11',
-        platform: 'Windows 2012 R2'
+        platform: 'Windows 8'
         , base: 'SauceLabs'
     },
     {browserName: 'safari', version: '8', platform: 'Mac 10.10', base: 'SauceLabs'},
     {
         browserName: 'firefox',
         version: '38',
-        platform: 'Windows 2012'
+        platform: 'Windows 7'
         , base: 'SauceLabs'
     },
     {
         browserName: 'firefox',
         version: '39',
-        platform: 'Windows 2012'
-        , base: 'SauceLabs'
-    },
-    {
-        browserName: 'chrome',
-        version: '40',
-        platform: 'Windows 2012'
+        platform: 'Windows 7'
         , base: 'SauceLabs'
     },
     {
         browserName: 'chrome',
         version: '41',
-        platform: 'Windows 2012'
+        platform: 'Windows 7'
         , base: 'SauceLabs'
     },
     {
         browserName: 'chrome',
         version: '42',
-        platform: 'Windows 2012'
+        platform: 'Windows 7'
         , base: 'SauceLabs'
     },
     {
         browserName: 'chrome',
         version: '43',
-        platform: 'Windows 2012'
+        platform: 'Windows 7'
         , base: 'SauceLabs'
     },
-    {browserName: 'opera', version: '12', platform: 'Windows 2003', base: 'SauceLabs'},
+    {browserName: 'opera', version: '12', platform: 'Windows XP', base: 'SauceLabs'},
     {browserName: 'iphone', version: '6.1', platform: 'Mac 10.8', base: 'SauceLabs'},
     {browserName: 'iphone', version: '7.1', platform: 'Mac 10.9', base: 'SauceLabs'},
     {browserName: 'iphone', version: '8.3', platform: 'Mac 10.10', base: 'SauceLabs'},
@@ -135,5 +129,9 @@ var karmaOpts = {
         type: 'lcovonly',
         dir: process.cwd() + '/coverage/',
         subdir: '.'
-    }
+    },
+    browserDisconnectTimeout : 10000, // default 2000
+    browserDisconnectTolerance : 1, // default 0
+    browserNoActivityTimeout : 4*60*1000, //default 10000
+    captureTimeout : 4*60*1000 //default 60000
 };
